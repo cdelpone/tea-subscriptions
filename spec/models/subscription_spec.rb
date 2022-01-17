35c701e7ca4d7a15e50a9c93284c3b38e5262ca4
Christina Delpone <cdelpone@gmail.com>
2022-01-16 17:59:11 -0700

    test models

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationship' do
+    it { should belong_to :customer }
+  end
+
+  describe 'enum' do
+    let(:status) { %w[active cancelled] }
+    it 'has the right index' do
+      status.each_with_index do |sub, index|
+        expect(Subscription.statuses[sub]).to eq(index)
+      end
+    end
+
+    let(:frequency) { %w[weekly monthly annually] }
+    it 'has the right index' do
+      frequency.each_with_index do |sub, index|
+        expect(Subscription.frequencies[sub]).to eq(index)
+      end
+    end
+  end
 end
